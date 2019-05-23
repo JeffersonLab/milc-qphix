a560ca7ad49980b88713836c4685fc064da5dc22
Carleton DeTar <detar@physics.utah.edu>
2019-05-23 14:26:48 -0400

    Move contents to qphix_int.h

--- su3.h
+++ su3.h
@@ -1,18 +1,5 @@
 #ifndef _SU3_H
 #define _SU3_H
 
-#if (QPHIX_PrecisionInt == 1)
-typedef float SU3_Real;
-typedef float QPHIX_Real;
-#endif
-#if (QPHIX_PrecisionInt == 2)
-typedef double SU3_Real;
-typedef double QPHIX_Real;
-#endif
-
-#define QPHIX_real(a) (a).real
-#define QPHIX_imag(a) (a).imag
-#define QPHIX_elem_M(a,ic,jc) ((a)[ic][jc])
-#define Uelem(a,b) QPHIX_elem_M(*U,a,b)
 
 #endif
